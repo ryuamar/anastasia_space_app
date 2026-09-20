aff9e5c990fcc9bc4e41800f8e61b96d303f0361
Ryu Amar <vigetaxii@gmail.com>
2026-09-20 15:17:20 +0100

    sync dev/android/app/src/main/java/com/xotaku/anastasia/MainActivity.java

--- dev/android/app/src/main/java/com/xotaku/anastasia/MainActivity.java
+++ dev/android/app/src/main/java/com/xotaku/anastasia/MainActivity.java
@@ -1,5 +1,37 @@
 package com.xotaku.anastasia;
 
-import com.getcapacitor.BridgeActivity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebViewClient;
+
+import java.util.Locale;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        bridge.setWebViewClient(new BridgeWebViewClient(bridge) {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Uri url = request.getUrl();
+                String host = url.getHost();
+
+                if ("https".equalsIgnoreCase(url.getScheme()) && host != null) {
+                    host = host.toLowerCase(Locale.ROOT);
+
+                    if (host.equals("xotaku.com") || host.endsWith(".xotaku.com")) {
+                        return false;
+                    }
+                }
+
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+    }
+}
